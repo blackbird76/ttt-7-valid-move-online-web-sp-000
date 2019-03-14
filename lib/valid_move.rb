529c7a721a120ac33a21f4ca0ccdbbdf8816f851
Megan McCullough <mmccullough206@gmail.com>
2019-03-14 23:29:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,10 +8,8 @@ end
 
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  elsif board[index] == "X" || board[index] == "O"
     true
   else
-    false
+   false
   end
 end
