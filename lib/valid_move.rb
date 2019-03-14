3fdd9647c17289a8bb259c3ac4ca8060443d7ec4
Megan McCullough <mmccullough206@gmail.com>
2019-03-14 23:18:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 def valid_move?(board, index)
   if position_taken?(board, index) == false && index.between?(1, 9)
     true
-  else
+  elsif position_taken?(board, index) == true
     false
   end
 end
